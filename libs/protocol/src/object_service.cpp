eeb8a0d64db0615e7e272f60fcf6ded790286f1d
Mathis Logemann <mathisloge@gmail.com>
2024-04-28 09:21:08 +0200

    wip

--- libs/protocol/src/object_service.cpp
+++ libs/protocol/src/object_service.cpp
@@ -2,6 +2,7 @@
 #include <agrpc/asio_grpc.hpp>
 #include <grpcpp/server.h>
 #include <grpcpp/server_builder.h>
+#include <stdexec/execution.hpp>
 #include "quite/detail/awaitable_server.hpp"
 #include "quite/detail/rethrow.hpp"
 
@@ -11,9 +12,19 @@ namespace quite
 using RpcSayHello = awaitable_server_t<&quite::proto::ObjectService::AsyncService::RequestSayHello>;
 using RpcFindObject = awaitable_server_t<&quite::proto::ObjectService::AsyncService::RequestFindObject>;
 
+using RpcFindObjectSender = agrpc::ServerRPC<&quite::proto::ObjectService::AsyncService::RequestFindObject>;
+
 ObjectService::ObjectService(agrpc::GrpcContext &context, grpc::ServerBuilder &builder)
 {
     builder.RegisterService(&service_);
+
+    find_obj_ = agrpc::register_sender_rpc_handler<RpcFindObjectSender>(
+        context, service_, [this](RpcFindObjectSender &rpc, const RpcFindObjectSender::Request &request) {
+            return stdexec::then(stdexec::just(RpcFindObjectSender::Response{}), [this, &request](auto &&response) {
+                onFindObject(request, response);
+                return response;
+            });
+        });
 
     agrpc::register_awaitable_rpc_handler<RpcSayHello>(
         context,
@@ -26,16 +37,16 @@ ObjectService::ObjectService(agrpc::GrpcContext &context, grpc::ServerBuilder &b
         },
         RethrowFirstArg{});
 
-    agrpc::register_awaitable_rpc_handler<RpcFindObject>(
-        context,
-        service_,
-        [this](RpcFindObject &rpc, RpcFindObject::Request &request) -> asio::awaitable<void> {
-            proto::ObjectReply response;
-
-            onFindObject(request, response);
-            co_await rpc.finish(response, grpc::Status::OK);
-        },
-        RethrowFirstArg{});
+    //agrpc::register_awaitable_rpc_handler<RpcFindObject>(
+    //    context,
+    //    service_,
+    //    [this](RpcFindObject &rpc, RpcFindObject::Request &request) -> asio::awaitable<void> {
+    //        proto::ObjectReply response;
+//
+    //        onFindObject(request, response);
+    //        co_await rpc.finish(response, grpc::Status::OK);
+    //    },
+    //    RethrowFirstArg{});
 }
 
 ObjectService::~ObjectService() = default;
