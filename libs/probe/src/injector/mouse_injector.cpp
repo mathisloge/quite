821427908a8acd300aaddfc78e2f3b061e9cc70b
Mathis Logemann <mathisloge@gmail.com>
2024-05-11 19:57:40 +0200

    wip

---
+++ libs/probe/src/injector/mouse_injector.cpp
@@ -0,0 +1,38 @@
+#include "mouse_injector.hpp"
+#include <QCoreApplication>
+#include <QMouseEvent>
+
+namespace quite::probe
+{
+MouseInjector::MouseInjector()
+    : mouse_{QStringLiteral("TestingMouse"),
+             100,
+             QInputDevice::DeviceType::Mouse,
+             QPointingDevice::PointerType::Generic,
+             QInputDevice::Capability::MouseEmulation,
+             1,
+             2}
+{}
+
+MouseInjector::~MouseInjector() = default;
+
+void MouseInjector::click_object(QObject *target, QPointF local_click_point)
+{
+    auto ev = new QMouseEvent(QMouseEvent::Type::MouseButtonPress,
+                              local_click_point,
+                              QPointF{0, 0},
+                              Qt::MouseButton::LeftButton,
+                              Qt::MouseButton::LeftButton,
+                              {},
+                              &mouse_);
+    QCoreApplication::postEvent(target, ev);
+    ev = new QMouseEvent(QMouseEvent::Type::MouseButtonRelease,
+                         local_click_point,
+                         QPointF{0, 0},
+                         Qt::MouseButton::LeftButton,
+                         Qt::MouseButton::LeftButton,
+                         {},
+                         &mouse_);
+    QCoreApplication::postEvent(target, ev);
+}
+} // namespace quite::probe
