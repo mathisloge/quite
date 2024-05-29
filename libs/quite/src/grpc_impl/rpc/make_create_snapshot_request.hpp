90d47b46c6f8bf6ab7ed7aff0e74a8abb9aee076
Mathis Logemann <mathisloge@gmail.com>
2024-05-29 19:04:31 +0200

    more inline cpp

--- libs/quite/src/grpc_impl/rpc/make_create_snapshot_request.hpp
+++ libs/quite/src/grpc_impl/rpc/make_create_snapshot_request.hpp
@@ -1,34 +1,13 @@
 #pragma once
-#include <expected>
-#include <agrpc/client_rpc.hpp>
 #include <agrpc/grpc_context.hpp>
-#include <exec/task.hpp>
-#include <quite/errors.hpp>
-#include <quite/proto/probe.grpc.pb.h>
+#include <quite/async_result.hpp>
 #include <quite/remote_object.hpp>
-#include "error_helper.hpp"
+#include <quite/proto/probe.grpc.pb.h>
 
 namespace quite::grpc_impl
 {
-static exec::task<Result<proto::ImageResponse>> make_create_snapshot_request(agrpc::GrpcContext &grpc_context,
+AsyncResult<proto::ImageResponse> make_create_snapshot_request(agrpc::GrpcContext &grpc_context,
                                                                              proto::ProbeService::Stub &stub,
-                                                                             ObjectId id)
-{
-    using RPC = agrpc::ClientRPC<&proto::ProbeService::Stub::PrepareAsyncCreateScreenshot>;
-    grpc::ClientContext client_context;
-    client_context.set_deadline(std::chrono::system_clock::now() + std::chrono::seconds{5});
-    client_context.set_wait_for_ready(true);
-
-    proto::ScreenshotRequest request;
-    request.set_object_id(id);
-
-    proto::ImageResponse response;
-    const auto status = co_await RPC::request(grpc_context, stub, client_context, request, response);
-    if (status.ok())
-    {
-        co_return response;
-    }
-    co_return std::unexpected(status2error(status));
-}
+                                                                             ObjectId id);
 
 } // namespace quite::grpc_impl
---
+++ libs/quite/src/grpc_impl/rpc/make_find_object_request.cpp
@@ -0,0 +1,28 @@
+#include "make_find_object_request.hpp"
+#include <agrpc/client_rpc.hpp>
+#include "error_helper.hpp"
+
+namespace quite::grpc_impl
+{
+AsyncResult<proto::ObjectReply> make_find_object_request(agrpc::GrpcContext &grpc_context,
+                                                         proto::ProbeService::Stub &stub,
+                                                         std::string_view object_name)
+{
+    using RPC = agrpc::ClientRPC<&proto::ProbeService::Stub::PrepareAsyncFindObject>;
+    grpc::ClientContext client_context;
+    client_context.set_deadline(std::chrono::system_clock::now() + std::chrono::seconds{5});
+    client_context.set_wait_for_ready(true);
+
+    proto::ObjectRequest request;
+    request.mutable_query()->mutable_properties()->emplace("objectName", std::string{object_name});
+
+    proto::ObjectReply response;
+    const auto status = co_await RPC::request(grpc_context, stub, client_context, request, response);
+    if (status.ok())
+    {
+        co_return response;
+    }
+    co_return std::unexpected(status2error(status));
+}
+
+} // namespace quite::grpc_impl
