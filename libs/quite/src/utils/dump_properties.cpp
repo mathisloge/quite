95b4de9a447b645fca6beeab4ac9467615f18ac0
Mathis Logemann <mathisloge@gmail.com>
2024-06-30 19:18:36 +0200

    dump properties

--- libs/quite/src/utils/dump_properties.cpp
+++ libs/quite/src/utils/dump_properties.cpp
@@ -1,33 +1,101 @@
 #include "quite/utils/dump_properties.hpp"
+#include <unordered_set>
 #include <nlohmann/json.hpp>
+#include <spdlog/spdlog.h>
 
 using json = nlohmann::json;
 namespace quite
 {
+namespace
+{
+AsyncResult<nlohmann::json> dump_properties(std::unordered_set<ObjectId> &visited_objects,
+                                            const RemoteObjectPtr &remote_object,
+                                            const std::vector<std::string_view> &properties);
 
 struct ValueVisitor
 {
-    void operator()(auto &&value)
-    {}
+    std::unordered_set<ObjectId> &visited_objects;
+    const std::vector<std::string_view> &properties;
+    nlohmann::json &out;
 
-    void operator()(const std::shared_ptr<RemoteObject> &object)
-    {}
+    AsyncResult<void> operator()(auto &&value)
+    {
+        out = value;
+        co_return {};
+    }
 
-    void operator()(const xyz::indirect<ArrayObject> &array)
-    {}
+    AsyncResult<void> operator()(const RemoteObjectPtr &object)
+    {
+        spdlog::info("visit object {}", object->id());
+        auto props = co_await dump_properties(visited_objects, object, properties);
+        if (props.has_value())
+        {
+            out = props.value();
+            co_return {};
+        }
+        co_return std::unexpected(props.error());
+    }
+
+    AsyncResult<void> operator()(const xyz::indirect<ArrayObject> &array)
+    {
+        out = nlohmann::json::array();
+        for (auto &&value : array->values)
+        {
+            auto&& target = out.emplace_back(nlohmann::json::object());
+            spdlog::info("visit next value in array");
+            auto prop_result = co_await std::visit(ValueVisitor{visited_objects, properties, target}, value);
+            if (not prop_result.has_value())
+            {
+                co_return std::unexpected(prop_result.error());
+            }
+        }
+        co_return {};
+    }
 };
 
-AsyncResult<nlohmann::json> dump_properties(const RemoteObjectPtr &remote_object,
+AsyncResult<nlohmann::json> dump_properties(std::unordered_set<ObjectId> &visited_objects,
+                                            const RemoteObjectPtr &remote_object,
                                             const std::vector<std::string_view> &properties)
 {
-    nlohmann::json out;
+    if (visited_objects.contains(remote_object->id()))
+    {
+        spdlog::info("object with id {} already exists", remote_object->id());
+        co_return nlohmann::json{};
+    }
+    visited_objects.emplace(remote_object->id());
+
+    nlohmann::json out = nlohmann::json::object();
     auto props = co_await remote_object->fetch_properties(properties);
+    if (not props.has_value())
+    {
+        co_return std::unexpected(props.error());
+    }
 
     for (auto &&prop : *props)
     {
-        std::visit(ValueVisitor{}, *prop.second->value());
+        if (prop.second->value().has_value())
+        {
+            auto prop_result =
+                co_await std::visit(ValueVisitor{visited_objects, properties, out[prop.first]}, *prop.second->value());
+            if (not prop_result.has_value())
+            {
+                co_return std::unexpected(prop_result.error());
+            }
+        }
+        else
+        {
+            spdlog::info("no value for {}", prop.first);
+        }
     }
 
-    co_return {};
+    co_return out;
+}
+
+} // namespace
+AsyncResult<nlohmann::json> dump_properties(const RemoteObjectPtr &remote_object,
+                                            const std::vector<std::string_view> &properties)
+{
+    std::unordered_set<ObjectId> objects;
+    co_return co_await dump_properties(objects, remote_object, properties);
 }
 } // namespace quite
