462be04d0855749b46469b5f67d09b37f3812d27
Mathis Logemann <13556116+mathisloge@users.noreply.github.com>
2025-01-10 22:01:49 +0100

    format

--- python/quite/__init__.pyi
+++ python/quite/__init__.pyi
@@ -1,3 +1,14 @@
-from ._quite import Application as Application, RemoteObject as RemoteObject, __doc__ as __doc__, __version__ as __version__
+from ._quite import (
+    Application as Application,
+)
+from ._quite import (
+    RemoteObject as RemoteObject,
+)
+from ._quite import (
+    __doc__ as __doc__,
+)
+from ._quite import (
+    __version__ as __version__,
+)
 
-__all__ = ['Application', 'RemoteObject', '__doc__', '__version__']
+__all__ = ["Application", "RemoteObject", "__doc__", "__version__"]
--- python/quite/_quite.pyi
+++ python/quite/_quite.pyi
@@ -4,11 +4,13 @@ __version_git_ref__: str
 class Application:
     def __init__(self, application_path: str) -> None:
         """__init__(self: quite._quite.Application, application_path: str) -> None"""
+
     def exit(self) -> None:
         """exit(self: quite._quite.Application) -> None
 
         Request to exit the application.
         """
+
     def find_object(self, object_query: str) -> RemoteObject:
         """find_object(self: quite._quite.Application, object_query: str) -> quite._quite.RemoteObject
 
@@ -18,5 +20,6 @@ class Application:
 class RemoteObject:
     def __init__(self, *args, **kwargs) -> None:
         """Initialize self.  See help(type(self)) for accurate signature."""
+
     @property
     def id(self) -> int: ...
