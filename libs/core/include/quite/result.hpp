36e655adf7b1564495c760704ebc942b8d117bc1
Mathis Logemann <13556116+mathisloge@users.noreply.github.com>
2025-01-12 13:24:47 +0100

    wip

--- libs/core/include/quite/result.hpp
+++ libs/core/include/quite/result.hpp
@@ -6,4 +6,10 @@ namespace quite
 {
 template <typename T>
 using Result = std::expected<T, Error>;
+
+template <typename T>
+constexpr Result<T> make_error_result(const ErrorCode code, std::string message)
+{
+    return std::unexpected{Error{.code = code, .message = std::forward<std::string>(message)}};
 }
+} // namespace quite
