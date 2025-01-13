7f228381f92d620d60f77d14c7a5a52caedad137
Mathis Logemann <13556116+mathisloge@users.noreply.github.com>
2025-01-13 19:14:19 +0100

    move code around

--- libs/probeqt/qt_meta_registry.cpp
+++ libs/probeqt/qt_meta_registry.cpp
@@ -75,19 +75,8 @@ Result<meta::Type> convert_enum_type(QMetaType type)
     return meta_enum;
 }
 
-Result<meta::Type> convert(QMetaType type)
+Result<meta::Type> convert_object_type(QMetaType type)
 {
-    const auto type_specifier = type.flags();
-    type_specifier.testFlag(QMetaType::TypeFlag::IsEnumeration);
-    type_specifier.testFlag(QMetaType::TypeFlag::IsGadget);
-    type_specifier.testFlag(QMetaType::TypeFlag::PointerToGadget);
-    type_specifier.testFlag(QMetaType::TypeFlag::PointerToQObject);
-
-    if (type_specifier.testFlag(QMetaType::TypeFlag::IsEnumeration))
-    {
-        return convert_enum_type(type);
-    }
-
     QVariant meta_type_instance;
     if (type.flags().testFlag(QMetaType::TypeFlag::NeedsConstruction))
     {
@@ -103,38 +92,47 @@ Result<meta::Type> convert(QMetaType type)
     }
 
     const QMetaObject *meta_object = type.metaObject();
-    if (meta_object != nullptr)
+    if (meta_object == nullptr)
     {
-        std::unique_ptr<meta::ObjectType> obj = std::make_unique<meta::ObjectType>();
-        obj->name = meta_object->className();
-        obj->id = type.id();
-
-        if (meta_object->superClass() != nullptr)
-        {
-            obj->superclass = meta_object->superClass()->metaType().id();
-        }
-
-        std::ranges::for_each(
-            std::ranges::iota_view(0, meta_object->propertyCount()) | std::views::transform([&](int prop_idx) {
-                return meta_object->property(prop_idx);
-            }) | std::views::transform(from_qmetaproperty),
-            [&obj](auto &&property) { obj->properties.emplace_back(std::forward<decltype(property)>(property)); });
-
-        std::ranges::for_each(
-            std::ranges::iota_view(0, meta_object->constructorCount()) | std::views::transform([&](int prop_idx) {
-                return meta_object->constructor(prop_idx);
-            }) | std::views::transform(from_qmetamethod),
-            [&obj](auto &&method) { obj->constructors.emplace_back(std::forward<decltype(method)>(method)); });
-
-        std::ranges::for_each(
-            std::ranges::iota_view(0, meta_object->methodCount()) | std::views::transform([&](int prop_idx) {
-                return meta_object->method(prop_idx);
-            }) | std::views::transform(from_qmetamethod),
-            [&obj](auto &&method) { obj->methods.emplace_back(std::forward<decltype(method)>(method)); });
-
-        return meta::Type{std::move(obj)};
+        return make_error_result<meta::Type>(ErrorCode::failed_precondition,
+                                             "Could not create a meta type from object");
     }
-    return make_error_result<meta::Type>(ErrorCode::failed_precondition, "Could not create a meta type from object");
+    std::unique_ptr<meta::ObjectType> obj = std::make_unique<meta::ObjectType>();
+    obj->name = meta_object->className();
+    obj->id = type.id();
+
+    if (meta_object->superClass() != nullptr)
+    {
+        obj->superclass = meta_object->superClass()->metaType().id();
+    }
+
+    std::ranges::for_each(
+        std::ranges::iota_view(0, meta_object->propertyCount()) | std::views::transform([&](int prop_idx) {
+            return meta_object->property(prop_idx);
+        }) | std::views::transform(from_qmetaproperty),
+        [&obj](auto &&property) { obj->properties.emplace_back(std::forward<decltype(property)>(property)); });
+
+    std::ranges::for_each(
+        std::ranges::iota_view(0, meta_object->constructorCount()) | std::views::transform([&](int prop_idx) {
+            return meta_object->constructor(prop_idx);
+        }) | std::views::transform(from_qmetamethod),
+        [&obj](auto &&method) { obj->constructors.emplace_back(std::forward<decltype(method)>(method)); });
+
+    std::ranges::for_each(std::ranges::iota_view(0, meta_object->methodCount()) |
+                              std::views::transform([&](int prop_idx) { return meta_object->method(prop_idx); }) |
+                              std::views::transform(from_qmetamethod),
+                          [&obj](auto &&method) { obj->methods.emplace_back(std::forward<decltype(method)>(method)); });
+
+    return meta::Type{std::move(obj)};
+}
+
+Result<meta::Type> convert(QMetaType type)
+{
+    if (type.flags().testFlag(QMetaType::TypeFlag::IsEnumeration))
+    {
+        return convert_enum_type(type);
+    }
+    return convert_object_type(type);
 }
 } // namespace
 
