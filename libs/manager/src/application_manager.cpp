617db060e904b5836d64c0b45191efb4ff6da340
Mathis Logemann <mathisloge@gmail.com>
2025-03-10 16:10:33 +0100

    wip

--- libs/manager/src/application_manager.cpp
+++ libs/manager/src/application_manager.cpp
@@ -28,6 +28,17 @@ ApplicationManager::ApplicationManager(asio2exec::asio_context &context)
     : impl_{std::make_unique<Impl>(context)}
 {}
 
+Result<ApplicationManager::ApplicationHandle> ApplicationManager::application(const Id &name)
+{
+    auto it = impl_->applications_.find(name);
+    if (it == impl_->applications_.end())
+    {
+        return make_error_result<ApplicationHandle>(
+            ErrorCode::not_found, fmt::format("Application with id '{}' is not present (anymore)", name));
+    }
+    return ApplicationHandle{it->second};
+}
+
 ApplicationManager::ApplicationHandle ApplicationManager::launch_application(
     Id id_name,
     const std::string &path_to_application,
@@ -55,7 +66,7 @@ ApplicationManager::ApplicationHandle ApplicationManager::launch_application(
     const std::vector<std::string> &args,
     const std::unordered_map<std::string, std::string> &environment)
 {
-    bp::process process{impl_->context_.get_executor(), path_to_application, args, environment};
+    bp::process process{impl_->context_.get_executor().get_executor(), path_to_application, args, environment};
     auto [app, emplaced] =
         impl_->applications_.emplace(std::move(id_name), std::make_shared<ApplicationImpl>(std::move(process)));
     // TODO: add preload of qt probe
@@ -70,7 +81,7 @@ ApplicationManager::ApplicationHandle ApplicationManager::launch_application(
  */
 ApplicationManager::ApplicationHandle ApplicationManager::attach_probe_to_running(QtProbe, Id id_name, PID process_id)
 {
-    bp::process process{impl_->context_.get_executor(), static_cast<bp::pid_type>(process_id)};
+    bp::process process{impl_->context_.get_executor().get_executor(), static_cast<bp::pid_type>(process_id)};
     auto [app, emplaced] =
         impl_->applications_.emplace(std::move(id_name), std::make_shared<ApplicationImpl>(std::move(process)));
     return ApplicationHandle{app->second};
@@ -84,7 +95,7 @@ ApplicationManager::ApplicationHandle ApplicationManager::attach_probe_to_runnin
  */
 ApplicationManager::ApplicationHandle ApplicationManager::attach_to_running(Id id_name, PID process_id)
 {
-    bp::process process{impl_->context_.get_executor(), static_cast<bp::pid_type>(process_id)};
+    bp::process process{impl_->context_.get_executor().get_executor(), static_cast<bp::pid_type>(process_id)};
     auto [app, emplaced] =
         impl_->applications_.emplace(std::move(id_name), std::make_shared<ApplicationImpl>(std::move(process)));
     return ApplicationHandle{app->second};
