3b4b3bf666d2beed652b96cb3684439513134fca
Mathis Logemann <13556116+mathisloge@users.noreply.github.com>
2025-04-27 12:48:28 +0200

    [qt_probe] add object tracker container check

--- libs/probeqt/tests/test_object_tracker.cpp
+++ libs/probeqt/tests/test_object_tracker.cpp
@@ -69,7 +69,7 @@ TEST_CASE("Find object by query", "[Qt,ObjectTracker]")
     REQUIRE(loop.processEvents());
 
     auto query_obj1 = std::make_shared<ObjectQuery>();
-    query_obj1->properties = {{"objectName", "test_obj1"}};
+    query_obj1->properties = {{"objectName", std::string{"test_obj1"}}};
 
     auto find_obj_copy =
         object_tracker.find_object_by_query(ObjectQuery{.properties = {{"objectName", std::string{"test_obj2"}}}});
@@ -82,4 +82,37 @@ TEST_CASE("Find object by query", "[Qt,ObjectTracker]")
 
     REQUIRE(find_obj_with_container.has_value());
     REQUIRE(find_obj_with_container->object_id == to_object_id(&test_obj2));
+}
+
+TEST_CASE("Find object by query with multiple parents", "[Qt,ObjectTracker]")
+{
+    QEventLoop loop;
+    ObjectTracker object_tracker;
+
+    QObject test_obj1{};
+    test_obj1.setObjectName("test_obj1");
+    object_tracker.add_object(&test_obj1);
+
+    QObject test_obj2{&test_obj1};
+    test_obj2.setObjectName("test_obj2");
+    object_tracker.add_object(&test_obj2);
+
+    QObject test_obj3{&test_obj2};
+    test_obj3.setObjectName("test_obj3");
+    object_tracker.add_object(&test_obj3);
+
+    QObject test_obj3_cpy{};
+    test_obj3_cpy.setObjectName("test_obj3");
+    object_tracker.add_object(&test_obj3_cpy);
+
+    REQUIRE(loop.processEvents());
+
+    auto query_obj1 = std::make_shared<ObjectQuery>();
+    query_obj1->properties = {{"objectName", std::string{"test_obj1"}}};
+
+    auto query_obj3 = object_tracker.find_object_by_query(
+        ObjectQuery{.container = query_obj1, .properties = {{"objectName", std::string{"test_obj3"}}}});
+
+    REQUIRE(query_obj3.has_value());
+    REQUIRE(query_obj3->object_id == to_object_id(&test_obj3));
 }
