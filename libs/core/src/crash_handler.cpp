f77cca7302f3a3c4e96348f1fbbafd0ce62f516f
Mathis Logemann <13556116+mathisloge@users.noreply.github.com>
2025-06-03 21:17:53 +0200

    CRASH TRACES

---
+++ libs/core/src/crash_handler.cpp
@@ -0,0 +1,88 @@
+#include "quite/crash_handler.hpp"
+#include <csignal>
+#include <cstring>
+#include <cpptrace/cpptrace.hpp>
+#include <signal.h>
+#include <sys/wait.h>
+#include <unistd.h>
+
+namespace quite
+{
+namespace
+{
+struct pipe_t
+{
+    union {
+        struct
+        {
+            int read_end;
+            int write_end;
+        };
+        int data[2];
+    };
+};
+static_assert(sizeof(pipe_t) == 2 * sizeof(int), "Unexpected struct packing");
+
+void handler(int signo, siginfo_t *info, void *context)
+{
+    const char *message = "SIGSEGV occurred:\n";
+    write(STDERR_FILENO, message, strlen(message));
+    cpptrace::frame_ptr buffer[200];
+    std::size_t count = cpptrace::safe_generate_raw_trace(buffer, 200);
+    pipe_t input_pipe{};
+    pipe(input_pipe.data);
+    const pid_t pid = fork();
+    if (pid == -1)
+    {
+        const char *fork_failure_message = "fork() failed\n";
+        write(STDERR_FILENO, fork_failure_message, strlen(fork_failure_message));
+        _exit(1);
+    }
+    if (pid == 0)
+    { // child
+        dup2(input_pipe.read_end, STDIN_FILENO);
+        close(input_pipe.read_end);
+        close(input_pipe.write_end);
+        execl("crash-tracer", "crash-tracer", nullptr);
+        const char *exec_failure_message = "exec(crash-tracer) failed: Make sure the crash-tracer executable is in "
+                                           "the current working directory and the binary's permissions are correct.\n";
+        write(STDERR_FILENO, exec_failure_message, strlen(exec_failure_message));
+        _exit(1);
+    }
+    for (std::size_t i = 0; i < count; i++)
+    {
+        cpptrace::safe_object_frame frame;
+        cpptrace::get_safe_object_frame(buffer[i], &frame);
+        write(input_pipe.write_end, &frame, sizeof(frame));
+    }
+    close(input_pipe.read_end);
+    close(input_pipe.write_end);
+    waitpid(pid, nullptr, 0);
+    _exit(1);
+}
+
+void warmup_cpptrace()
+{
+    cpptrace::frame_ptr buffer[10];
+    std::size_t count = cpptrace::safe_generate_raw_trace(buffer, 10);
+    cpptrace::safe_object_frame frame;
+    cpptrace::get_safe_object_frame(buffer[0], &frame);
+}
+} // namespace
+
+void register_crash_handler()
+{
+    cpptrace::absorb_trace_exceptions(false);
+    cpptrace::register_terminate_handler();
+    warmup_cpptrace();
+
+    struct sigaction action = {0};
+    action.sa_flags = 0;
+    action.sa_sigaction = &handler;
+    if (sigaction(SIGSEGV, &action, NULL) == -1)
+    {
+        perror("sigaction");
+        exit(EXIT_FAILURE);
+    }
+}
+} // namespace quite
