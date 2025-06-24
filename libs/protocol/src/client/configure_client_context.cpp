8915249e873c07c0a32c8297fa5b1fdd93085cf8
Mathis Logemann <13556116+mathisloge@users.noreply.github.com>
2025-06-24 21:45:17 +0200

    increase grpc timeout

--- libs/protocol/src/client/configure_client_context.cpp
+++ libs/protocol/src/client/configure_client_context.cpp
@@ -4,7 +4,7 @@ namespace quite::proto
 {
 void configure_client_context(grpc::ClientContext &client_context)
 {
-    client_context.set_deadline(std::chrono::system_clock::now() + std::chrono::seconds{5});
+    client_context.set_deadline(std::chrono::system_clock::now() + std::chrono::seconds{30});
     client_context.set_wait_for_ready(true);
 }
 } // namespace quite::proto
