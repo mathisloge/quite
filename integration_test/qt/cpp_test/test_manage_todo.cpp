39bafd487eb75cf18d6f8ee821b00897e6e6e0d1
Mathis Logemann <13556116+mathisloge@users.noreply.github.com>
2025-07-26 11:15:48 +0200

    restructure bdd cpp impl

--- integration_test/qt/cpp_test/test_manage_todo.cpp
+++ integration_test/qt/cpp_test/test_manage_todo.cpp
@@ -14,32 +14,27 @@ static suite<"integration manage todo"> _ = [] { // NOLINT
         steps.feature("*") = [&] {
             auto probe = probe_manager.launch_probe_application("test_application", kTestApplicationPath);
             probe.wait_for_connected(std::chrono::seconds{5});
-            steps.scenario("*") = [&] {
-                steps.given("I have entered '{todoText}' into the todo input field") = [&](std::string todoText) {
-                    auto input_obj =
-                        probe.find_object(quite::make_query().with_property("objectName", std::string{"inputField"}));
-                    input_obj.property("text").write(std::move(todoText));
+            steps.scenario("*") = [&] {};
+            steps.given("I have entered '{todoText}' into the todo input field") = [&](std::string todoText) {
+                auto input_obj =
+                    probe.find_object(quite::make_query().with_property("objectName", std::string{"inputField"}));
+                input_obj.property("text").write(std::move(todoText));
+            };
+            steps.when("I click the 'Add' button") = [&]() {
+                auto btn_query = quite::make_query().with_property("objectName", std::string{"addButton"});
+                auto btn_obj = probe.find_object(btn_query);
+                btn_obj.mouse_action();
+            };
 
-                    steps.when("I click the 'Add' button") = [&]() {
-                        auto btn_query = quite::make_query().with_property("objectName", std::string{"addButton"});
-                        auto btn_obj = probe.find_object(btn_query);
-                        btn_obj.mouse_action();
-                        steps.then("the todo list should display '{expectdTodoText}' as a new item") =
-                            [&](std::string todoText) {
-                                auto list_query =
-                                    quite::make_query().with_property("objectName", std::string{"listView"});
-                                auto list_obj = probe.find_object(list_query);
+            steps.then("the todo list should display '{expectdTodoText}' as a new item") = [&](std::string todoText) {
+                auto list_query = quite::make_query().with_property("objectName", std::string{"listView"});
+                auto list_obj = probe.find_object(list_query);
 
-                                auto delegate_query =
-                                    quite::make_query().with_property("text", todoText).with_type("SwipeDelegate");
-                                // delegate_query->container = list_query;
-                                auto delegate_index =
-                                    probe.try_find_object(delegate_query, std::chrono::seconds{2}).property("text");
-                                expect(std::holds_alternative<std::string>(delegate_index.value()));
-                                expect(that % todoText == std::get<std::string>(delegate_index.value()));
-                            };
-                    };
-                };
+                auto delegate_query = quite::make_query().with_property("text", todoText).with_type("SwipeDelegate");
+                // delegate_query->container = list_query;
+                auto delegate_index = probe.try_find_object(delegate_query, std::chrono::seconds{2}).property("text");
+                expect(std::holds_alternative<std::string>(delegate_index.value()));
+                expect(that % todoText == std::get<std::string>(delegate_index.value()));
             };
         };
     };
