59e01df338c74815923207bdd6126c66d2e68f9e
Mathis Logemann <13556116+mathisloge@users.noreply.github.com>
2025-07-27 20:43:24 +0200

    feat: add MIT license

--- test/probeqt/main.cpp
+++ test/probeqt/main.cpp
@@ -1,7 +1,3 @@
-// SPDX-FileCopyrightText: 2024 Mathis Logemann <mathisloge.opensource@pm.me>
-//
-// SPDX-License-Identifier: GPL-3.0-or-later
-
 #include <QCoreApplication>
 #include <catch2/catch_session.hpp>
 #include <quite/setup_logger.hpp>
