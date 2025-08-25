8cc08cd5ebe7ab2ce24b2f4e11b68ab75c91645e
Mathis Logemann <13556116+mathisloge@users.noreply.github.com>
2025-08-25 22:06:08 +0200

    feat: squash this

--- libs/probeqt/impl/property_collector.hpp
+++ libs/probeqt/impl/property_collector.hpp
@@ -18,6 +18,8 @@ struct ObjectMeta
 
     [[nodiscard]] static ObjectMeta from_qobject(QObject *object);
 };
+[[nodiscard]] entt::meta_any convert_void_ptr_to_any(QMetaType meta_type, void *data_ptr);
+
 [[nodiscard]] std::pair<std::string, entt::meta_any> read_property(QVariant property_value,
                                                                    const QMetaProperty &property);
 Result<void> write_property(const ObjectMeta &meta,
