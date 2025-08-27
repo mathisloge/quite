7c96af2cb08555d8f7adafe995ec4ef2c406b069
Mathis Logemann <13556116+mathisloge@users.noreply.github.com>
2025-08-27 20:27:30 +0200

    feat(probeqt): properly read out enum values

--- libs/probeqt/tests/test_property_collector.cpp
+++ libs/probeqt/tests/test_property_collector.cpp
@@ -11,6 +11,7 @@
 #include <quite/error.hpp>
 #include <quite/value/object_query.hpp>
 #include "property_collector.hpp"
+#include "quite/value/generic_value_class.hpp"
 
 using namespace quite;
 using namespace boost::ut;
@@ -27,6 +28,34 @@ class SimpleObject : public QObject
     int m_number{42};
     QString m_text{"hello"};
 };
+
+class SimpleQGadget
+{
+    Q_GADGET
+    Q_PROPERTY(Color color MEMBER color_)
+    Q_PROPERTY(Color2 color2 MEMBER color2_)
+  public:
+    enum class Color
+    {
+        Red,
+        Green,
+        Blue
+    };
+    Q_ENUM(Color)
+
+    enum class Color2 : std::uint64_t
+    {
+        Red,
+        Green,
+        Blue
+    };
+    Q_ENUM(Color2)
+
+  private:
+    Color color_;
+    Color2 color2_;
+};
+
 } // namespace
 
 static suite<"qtprobe propertycollector"> _ = [] { // NOLINT
@@ -70,6 +99,20 @@ static suite<"qtprobe propertycollector"> _ = [] { // NOLINT
         expect(!props.contains("number"));
         expect(props.contains("text"));
         expect(props["text"].cast<QString>().toStdString() == "hello"sv);
+    };
+
+    "convert_void_ptr_to_any of an QGadget with enum values"_test = [] {
+        SimpleQGadget obj{};
+
+        auto any_obj = convert_void_ptr_to_any(obj.staticMetaObject.metaType(), &obj);
+        expect(any_obj.allow_cast<GenericClass>());
+        auto generic_class = any_obj.cast<GenericClass>();
+
+        expect(generic_class.properties.contains("color"));
+        expect(generic_class.properties["color"].allow_cast<std::int64_t>());
+
+        expect(generic_class.properties.contains("color2"));
+        expect(generic_class.properties["color2"].allow_cast<std::uint64_t>());
     };
 };
 
